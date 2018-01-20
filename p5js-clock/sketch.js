3b66b702e995215bd45138402b8dc754ad8557d4
digitalight <mikeglover06@gmail.com>
2018-01-20 20:06:07 +0000

    Added markings

--- p5js-clock/sketch.js
+++ p5js-clock/sketch.js
@@ -4,16 +4,29 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   clocksize = windowHeight/1.5;
+  translate(width/2,height/2);
+  rotate(-90);
 
 }
 
 function draw() {
   sc = second();
   background(0);
-  translate(width/2,height/2);
-  rotate(-90);
   let mn = minute() + (sc / 60);
   let hr = hour() + (mn / 60);
+
+  translate(width/2,height/2);
+  rotate(-90);
+
+  //markings
+  strokeWeight(2);
+  stroke(255);
+  for (ma=0; ma < 12; ma++){
+    push();
+      rotate(30*ma);
+      line(0,clocksize-(clocksize/1.6),0,clocksize-(clocksize/1.65));
+    pop();
+  }
 
 
   strokeWeight(clocksize/50);
@@ -38,8 +51,9 @@ function draw() {
   rotate(hand2);
   line(0,0,clocksize-(clocksize/1.4),0);
   pop();
-  
+
   //seconds
+  strokeWeight(clocksize/100);
   stroke(255,0,120);
   let hand1 = map (sc, 0, 60, 0, 360);
   arc(0,0, clocksize, clocksize, 0, hand1);
@@ -49,7 +63,10 @@ function draw() {
   line(0,0,clocksize-(clocksize/1.5),0);
   pop();
 
-
+  //point
+  strokeWeight(8);
+  stroke(255);
+  point(0,0);
 
   // fill(255);
   // noStroke();
